9fc90edbd30217bfc50a7697949b2a8c75ea6428
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-18 18:02:23 +0100

    Update build.gradle.kts

--- CarateenProvider/build.gradle.kts
+++ CarateenProvider/build.gradle.kts
@@ -1,6 +1,44 @@
+import com.android.build.gradle.LibraryExtension
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
+apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+configure<CloudstreamExtension> {
+    // سيقوم النظام بجلب اسم المستودع تلقائياً
+    setRepo(System.getenv("GITHUB_REPOSITORY") ?: "youssef_cartoon")
+}
+
+configure<LibraryExtension> {
+    namespace = "com.momen.carateen"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+// حل مشكلة jvmTarget و Unresolved reference: kotlinOptions
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        // إيقاف التحذيرات التي قد تعيق البناء
+        freeCompilerArgs = freeCompilerArgs + "-Xjvm-default=all"
+    }
+}
+
 dependencies {
-    // قمنا بحذف السطر الذي يبدأ بـ val cloudstream لأن النظام تعرف عليه تلقائياً
+    // استخدمنا الاسم مباشرة لتجنب خطأ "already exists"
     add("cloudstream", "com.github.lagradost:cloudstream3:master-SNAPSHOT")
+    
+    // المكتبات المساعدة لجلب البيانات وتحليل النصوص
     add("implementation", "org.jsoup:jsoup:1.17.2")
     add("implementation", "com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
 }
