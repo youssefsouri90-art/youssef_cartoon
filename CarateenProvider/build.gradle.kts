c67b40ca4e81fc70474ad08ad635ae0c678d104e
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 05:27:00 +0100

    Update build.gradle.kts

--- CarateenProvider/build.gradle.kts
+++ CarateenProvider/build.gradle.kts
@@ -1,45 +1,48 @@
-import com.android.build.gradle.LibraryExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.github.lagradost:cloudstream3-gradle:master-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
 }
 
-configure<LibraryExtension> {
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
+apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+configure<CloudstreamExtension> {
+    // يجب أن يطابق مسار الكلاس في ملف CarateenPlugin.kt
+    setProviderClass("com.momen.CarateenPlugin")
+}
+
+android {
     namespace = "com.momen.carateen"
     compileSdk = 34
-    defaultConfig { minSdk = 21 }
+
+    defaultConfig {
+        minSdk = 21
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-}
 
-// حل عبقري: إنشاء الملف المفقود عند بداية أي عملية بناء لإسكات Gradle
-tasks.configureEach {
-    if (name.contains("prepare", ignoreCase = true) || name.contains("generate", ignoreCase = true)) {
-        doLast {
-            val typedefsFile = file("build/intermediates/annotations_typedef_file/debug/typedefs.txt")
-            if (!typedefsFile.exists()) {
-                typedefsFile.parentFile.mkdirs()
-                typedefsFile.writeText("")
-            }
-        }
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
-    // تعطيل المهمة المزعجة إذا وجدت
-    if (name == "extractDebugAnnotations") {
-        enabled = false
-    }
-}
-
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions { jvmTarget = "1.8" }
 }
 
 dependencies {
-    "compileOnly"("com.github.lagradost:cloudstream3:v3.0.1")
-    "implementation"("org.jsoup:jsoup:1.17.2")
-    "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+    val cloudstreamVersion = "v3.0.1"
+    compileOnly("com.github.lagradost:cloudstream3:$cloudstreamVersion")
+    implementation("org.jsoup:jsoup:1.17.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
 }
