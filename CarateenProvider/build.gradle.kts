13d869f9d16a70cc4749ec6fbcc3e12aaa346b3f
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 02:41:58 +0100

    Update build.gradle.kts

--- CarateenProvider/build.gradle.kts
+++ CarateenProvider/build.gradle.kts
@@ -1,13 +1,20 @@
 import com.android.build.gradle.LibraryExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-apply(plugin = "com.android.library")
-apply(plugin = "kotlin-android")
+// تفعيل الإضافات الأساسية للأندرويد وكوتلن
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
 
 configure<LibraryExtension> {
     namespace = "com.momen.carateen"
     compileSdk = 34
-    defaultConfig { minSdk = 21 }
+
+    defaultConfig {
+        minSdk = 21
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -15,20 +22,14 @@ configure<LibraryExtension> {
 }
 
 tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions { jvmTarget = "1.8" }
-}
-
-dependencies {
-    // جلب المكتبة من JitPack مباشرة (هذا الرابط يعمل دائماً)
-    compileOnly("com.github.lagradost:cloudstream3:master-SNAPSHOT")
-    implementation("org.jsoup:jsoup:1.17.2")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
-}
-
-// إنشاء أمر "make" يدوياً بما أن الأصلي معطل
-tasks.register("make") {
-    dependsOn("assembleDebug")
-    doLast {
-        println("تهانينا! تم بناء إضافة كاراطين بنجاح.")
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
+}
+
+// استخدام الطريقة الكاملة لإضافة المكتبات لحل مشكلة Unresolved reference
+dependencies {
+    "compileOnly"("com.github.lagradost:cloudstream3:master-SNAPSHOT")
+    "implementation"("org.jsoup:jsoup:1.17.2")
+    "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
 }
