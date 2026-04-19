f9375a196af5b872de6509d1086605a933a56761
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 02:01:23 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,15 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        // تغيير النسخة هنا لضمان عدم فشل التحميل من GitHub
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
     }
 }
 
-// ملاحظة: لا تضع بلوك allprojects هنا لمنع التعارض مع settings.gradle.kts
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
