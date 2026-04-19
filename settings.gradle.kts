bfa3030b65d1fb8b5994d38aee84cc6175dde083
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 03:19:30 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,11 +7,15 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    // إجبار المشروع على استخدام الإعدادات المذكورة هنا فقط لمنع تعارض الصلاحيات
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
+        // المستودع الأساسي لجلب مكتبة Cloudstream
         maven { url = uri("https://jitpack.io") }
     }
 }
+
+rootProject.name = "youssef_cartoon"
 include(":CarateenProvider")
