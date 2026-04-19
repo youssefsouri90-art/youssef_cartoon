ae4d87cc0703b03d9f5b9d1788045f3ddba05f1e
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 06:09:41 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("com.github.lagradost:cloudstream3-gradle:master-SNAPSHOT")
+        // استخدام معرف التزام محدد لضمان التحميل بنجاح
+        classpath("com.github.lagradost:cloudstream3-gradle:604d588")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
