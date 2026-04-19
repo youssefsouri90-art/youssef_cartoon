7e50376b805be940a30c8d83fb7dbf5d28b351e9
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 05:15:26 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,22 @@
 plugins {
     id("com.android.library")
     id("kotlin-android")
-    // إضافة البلاجن المسؤول عن تحويل المكتبة إلى إضافة Cloudstream
+    // هذا البلجن هو الذي يحول الكود إلى صيغة الإضافات (Dex)
     id("com.lagradost.cloudstream3.gradle") version "1.0.0"
 }
 
 cloudstream {
-    // تحديد الكلاس الأساسي الذي يحتوي على منطق جلب البيانات
-    setProviderClass("com.momen.carateen.CarateenProvider")
+    // هنا نضع اسم الكلاس الذي يحتوي على @CloudstreamPlugin مع الباكج
+    setProviderClass("com.momen.CarateenPlugin")
 }
 
 android {
     namespace = "com.momen.carateen"
     compileSdk = 34
-    
-    defaultConfig {
-        minSdk = 21
-    }
+    defaultConfig { minSdk = 21 }
 }
 
 dependencies {
-    // المكتبات الضرورية التي يعتمد عليها التطبيق
     compileOnly("com.github.lagradost:cloudstream3:v3.0.1")
     implementation("org.jsoup:jsoup:1.17.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
