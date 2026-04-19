7d7a27f79f79ffd08cb731e016e8d865c03dced2
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 05:40:15 +0100

    Update build.gradle.kts

--- CarateenProvider/build.gradle.kts
+++ CarateenProvider/build.gradle.kts
@@ -1,24 +1,11 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("com.github.lagradost:cloudstream3-gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
 apply(plugin = "com.android.library")
 apply(plugin = "kotlin-android")
 apply(plugin = "com.lagradost.cloudstream3.gradle")
 
 configure<CloudstreamExtension> {
-    // يجب أن يطابق مسار الكلاس في ملف CarateenPlugin.kt
+    // تأكد أن هذا المسار يطابق الكلاس في ملف CarateenPlugin.kt
     setProviderClass("com.momen.CarateenPlugin")
 }
 
