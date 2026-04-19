dada8360d1321daeeb3bbc4b96ec6a48b7da19b5
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 02:53:41 +0100

    Update build.gradle.kts

--- CarateenProvider/build.gradle.kts
+++ CarateenProvider/build.gradle.kts
@@ -9,31 +9,16 @@ plugins {
 configure<LibraryExtension> {
     namespace = "com.momen.carateen"
     compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    
-    // هذا السطر يحل مشكلة الخطأ الأخير (extractDebugAnnotations)
-    buildFeatures {
-        resValues = true
-    }
-}
-
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    defaultConfig { minSdk = 21 }
 }
 
 dependencies {
-    // استخدمنا نسخة ثابتة (رقمية) بدلاً من SNAPSHOT لتجنب مشاكل التصريح
-    "compileOnly"("com.github.lagradost:cloudstream3:3.4.0")
+    // هذه النسخة متاحة للجميع ولا تتطلب تسجيل دخول (تتخطى خطأ 401)
+    "compileOnly"("com.lagradost:cloudstream3:3.2.1") 
     "implementation"("org.jsoup:jsoup:1.17.2")
     "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions { jvmTarget = "1.8" }
 }
