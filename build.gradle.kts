27d39343702da57676b5f7885fb5b12c84d9b6f4
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 10:00:05 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
-        // هذا هو الإصدار المستقر الذي حل المشكلة سابقاً
+        // استخدام هذا الإصدار تحديداً لأنه الأكثر استقراراً على سيرفرات JitPack
         classpath("com.github.lagradost:cloudstream3-gradle:6.0.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
