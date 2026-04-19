c32bc624abaa6ef0b3a5f1b60345e43d0f0b0daa
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 05:06:17 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,27 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    // إضافة البلاجن المسؤول عن تحويل المكتبة إلى إضافة Cloudstream
+    id("com.lagradost.cloudstream3.gradle") version "1.0.0"
+}
+
+cloudstream {
+    // تحديد الكلاس الأساسي الذي يحتوي على منطق جلب البيانات
+    setProviderClass("com.momen.carateen.CarateenProvider")
+}
+
+android {
+    namespace = "com.momen.carateen"
+    compileSdk = 34
+    
+    defaultConfig {
+        minSdk = 21
     }
 }
 
-// حذفنا allprojects من هنا لأننا وضعناها في settings.gradle.kts
+dependencies {
+    // المكتبات الضرورية التي يعتمد عليها التطبيق
+    compileOnly("com.github.lagradost:cloudstream3:v3.0.1")
+    implementation("org.jsoup:jsoup:1.17.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+}
