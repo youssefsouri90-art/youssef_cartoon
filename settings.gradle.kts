6bc7b2bff4954d530db59805d81f071ec88be5a2
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 03:30:16 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+// ملف: /settings.gradle.kts (في المجلد الرئيسي للمستودع)
+
 pluginManagement {
     repositories {
         google()
@@ -7,15 +9,18 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    // إجبار المشروع على استخدام الإعدادات المذكورة هنا فقط لمنع تعارض الصلاحيات
+    // هذا السطر ضروري جداً لمنع تعارض المستودعات وحل مشكلة الـ 401 Unauthorized
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
-        // المستودع الأساسي لجلب مكتبة Cloudstream
+        // المستودع الذي يحتوي على مكتبة Cloudstream الأساسية
         maven { url = uri("https://jitpack.io") }
     }
 }
 
+// اسم المستودع الخاص بك
 rootProject.name = "youssef_cartoon"
+
+// أهم سطر: ربط كود موقع كاراطين بعملية البناء
 include(":CarateenProvider")
