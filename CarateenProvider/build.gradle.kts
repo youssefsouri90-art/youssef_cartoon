5bd39e6ec6115ba19d46b28084541018fe8e0208
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 02:34:16 +0100

    Update build.gradle.kts

--- CarateenProvider/build.gradle.kts
+++ CarateenProvider/build.gradle.kts
@@ -1,8 +1,16 @@
 import com.android.build.gradle.LibraryExtension
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+// أعدنا تفعيل الإضافات الثلاث الأساسية
 apply(plugin = "com.android.library")
 apply(plugin = "kotlin-android")
+apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+configure<CloudstreamExtension> {
+    // هذا السطر يربط الإضافة بمستودعك على GitHub
+    setRepo(System.getenv("GITHUB_REPOSITORY") ?: "youssef_cartoon")
+}
 
 configure<LibraryExtension> {
     namespace = "com.momen.carateen"
@@ -25,8 +33,10 @@ tasks.withType<KotlinCompile>().configureEach {
 }
 
 dependencies {
-    // استخدام "add" يضمن أن Gradle سيفهم الأمر مهما كان إصداره
-    add("compileOnly", "com.github.lagradost:cloudstream3:master-SNAPSHOT")
+    // الطريقة الصحيحة والمجربة لربط المكتبة
+    val cloudstream by configurations.getting
+    add(cloudstream.name, "com.github.lagradost:cloudstream3:master-SNAPSHOT")
+    
     add("implementation", "org.jsoup:jsoup:1.17.2")
     add("implementation", "com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
 }
