0203c72b20380adf721a7b84c44a5a00f4072807
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-19 03:23:38 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,29 @@
+// ملف: /build.gradle.kts (المجلد الرئيسي)
+
 buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
+        maven { url = uri("https://jitpack.io") }
     }
     dependencies {
+        // تعريف إصدار الأندرويد وكوتلن على مستوى المشروع بالكامل
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        
+        // إضافة مكتبة Cloudstream لكي يتعرف عليها النظام في كل مكان
+        classpath("com.github.lagradost:cloudstream3:master-SNAPSHOT")
     }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
